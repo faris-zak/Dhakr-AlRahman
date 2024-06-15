5904f0ef1286d7abe8ae0d67f11bf792ccc7a1a4
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2024-06-16 00:50:16 +0400

    commit

--- adhkarScript.js
+++ adhkarScript.js
@@ -1,32 +1,69 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-      
-function filterFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
+function search_section(){
+  let input = document.getElementById('searchbar').value
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName('sections');
+  
+  for (i = 0; i < x.length; i++){
+    if (!x[i].innerHTML.toLowerCase().includes(input)){
+    x[i].style.display = 'none';
     }
+    else{
+    x[i].style.display = 'list-item';
+    }
+}};
+
+
+function clearField(){
+  document.getElementById('searchbar').value = '';
+}
+
+
+const searchbar = document.getElementById('searchbar');
+const clearButton = document.getElementById('clearButton');
+
+function updateButtonVisibility() {
+  if (searchbar.value === ''){
+    clearButton.style.opacity = 0;
+    clearButton.style.marginBottom = '0px';
+    searchbar.style.marginBottom = '0px';
+  }
+  else{
+    clearButton.style.opacity = 1;
+    clearButton.style.marginBottom = '50px';
+    searchbar.style.marginBottom = '10px';
   }
 }
 
-var myButton = document.getElementById("toTop");
+updateButtonVisibility();
+
+searchbar.addEventListener('input', updateButtonVisibility);
+
+clearButton.addEventListener('click', function(){
+  location.reload();
+});
+
+
+
+document.addEventListener('click', function(event){
+  const isClickInside = searchbar.contains(event.target); 
+  if (!isClickInside) {
+      searchbar.placeholder = 'ماذا تفضل أن تقرأ ؟';
+  } else {
+      searchbar.placeholder = 'إبحث...';
+  }
+});
+
+
+
+var myButton = document.getElementById('toTop');
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    myButton.style.opacity = "1";
+    myButton.style.opacity = '1';
   } else {
-    myButton.style.opacity = "0";
+    myButton.style.opacity = '0';
   }
 }
 
