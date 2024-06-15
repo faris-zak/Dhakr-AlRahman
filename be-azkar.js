20ade10d7984495a219e030ee2b81f4a0a95cea7
Al-Faris Mujahid AlZakwani <107947850+faris-zak@users.noreply.github.com>
2024-06-15 15:06:08 +0400

    Add files via upload

---
+++ be-azkar.js
@@ -0,0 +1,38 @@
+console.log("Connected Successfully.");
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+      
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
+var myButton = document.getElementById("toTop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.opacity = "1";
+  } else {
+    myButton.style.opacity = "0";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
