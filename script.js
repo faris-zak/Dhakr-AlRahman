816038b6599cd98e75854191ab72def857385f74
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2024-06-16 10:29:21 +0400

    commit

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const logo = document.getElementById("dhakrAlrahmanLogo");
 const navBarList = document.getElementById("navBarList");
 
 logo.addEventListener("click", () => {
-    if(navBarList.style.opacity === "1"){
-        navBarList.style.opacity = "0";
-    } else{
+    if(navBarList.style.opacity === "0"){
         navBarList.style.opacity = "1";
+    } else{
+        navBarList.style.opacity = "0";
     }
 });
